5095f3a5356b862a6a2ffe6fad734ef7ce3596ad
srb- <sbeath@gmail.com>
2015-02-19 15:23:43 -0500

    Awesome, got Express 4 going... yup! Next is publish to OpenShift?

--- NodejsWebApp1/README.md
+++ NodejsWebApp1/README.md
@@ -1,3 +1,17 @@
 ﻿# NodejsWebApp1
 
+- Express 4
+- publish to OpenShift
+- make sure OpenShift is SSL
+- split files and do modular thing
+- mongodb
+- bower
+- passport authentication
+- browser code
+- share data structures in node and browser
+- use Coinkite API
+- get promises going with that API
+
+
+
 
--- NodejsWebApp1/server.ts
+++ NodejsWebApp1/server.ts
@@ -1,6 +1,23 @@
-﻿import http = require('http');
-var port = process.env.port || 1337
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿//import http = require('http');
+//var port = process.env.port || 1337
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World from Express!!!')
+})
+
+var server = app.listen(1337, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Example app listening at http://%s:%s', host, port)
+
+})
