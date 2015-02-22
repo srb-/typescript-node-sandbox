9d6c196cdee2feaf6286e7eab527c07c9b42edd1
srb- <sbeath@gmail.com>
2015-02-22 13:59:48 -0500

    Very cool, got localhost SSL going.. see the instructions!

--- NodejsWebApp1/README.md
+++ NodejsWebApp1/README.md
@@ -25,6 +25,31 @@ https://help.openshift.com/hc/en-us/articles/202398810-How-to-redirect-traffic-t
 
 
 
+to get localhost.ssl going
+
+add to etc/hosts localhost.ssl 127.0.0.1.. don't forget sudo
+
+
+in backrailbars/scripts
+
+sudo openssl genrsa -des3 -out server.key 1024
+sudo openssl req -new -key server.key -out server.csr
+  use localhost.ssl for common name
+sudo cp server.key server.key.org
+sudo openssl rsa -in server.key.org -out server.key
+ sudo openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
+run ./start to start server in that mode
+click on lock, view cert, Copy to File as localhost.ssl.crt on Desktop... efaults are OK
+in Chrome, go to settings, search ssl, manage certifiates...
+go to trusted root certs
+add localhost.ssl.crt you copied to desktop ... import it, 
+import it into trusted roots
+ restart browser... but in Chrome you must go to the menu and click Exit for a real restart
+
+
+
+
 git pull openshift master
 git merge openshift master -s recursive -X ours
 git remote add opentshift ssh://blah
--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -5,18 +5,22 @@
 //    res.end('Hello World\n');
 //}).listen(port);
 var express = require('express');
-var pem = require('pem');
 var https = require('https');
+var fs = require('fs');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 443;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 // had to set set OPENSSL_CONF=c:/libs/openssl-0.9.8k/openssl.cnf to my Git's SSL folder on Windows. 
-pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
-    var app = express();
-    app.get('/', function (req, res) {
-        res.send('Hello World from Express!!!');
-    });
-    https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(server_port, server_ip_address, function () {
-        console.log('Example app listening at http://%s:%s', server_ip_address, server_port);
-    });
+var options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.crt'),
+    requestCert: false,
+    rejectUnauthorized: false
+};
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World from Express in SSL!!!');
+});
+https.createServer(options, app).listen(server_port, server_ip_address, function () {
+    console.log('Example app listening at https://%s:%s', server_ip_address, server_port);
 });
 //# sourceMappingURL=server.js.map
