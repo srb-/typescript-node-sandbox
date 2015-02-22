e16e72294b9ffbb080eeff6529c6ce59f4c6c2cd
srb- <sbeath@gmail.com>
2015-02-21 22:56:27 -0500

    Got a cert going but it changes every time you run... so modify to be my self signed.

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -5,13 +5,18 @@
 //    res.end('Hello World\n');
 //}).listen(port);
 var express = require('express');
-var app = express();
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 1337;
+var pem = require('pem');
+var https = require('https');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 443;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-app.get('/', function (req, res) {
-    res.send('Hello World from Express!!!');
-});
-var server = app.listen(server_port, server_ip_address, function () {
-    console.log('Example app listening at http://%s:%s', server_ip_address, server_port);
+// had to set set OPENSSL_CONF=c:/libs/openssl-0.9.8k/openssl.cnf to my Git's SSL folder on Windows. 
+pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+    var app = express();
+    app.get('/', function (req, res) {
+        res.send('Hello World from Express!!!');
+    });
+    https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(server_port, server_ip_address, function () {
+        console.log('Example app listening at http://%s:%s', server_ip_address, server_port);
+    });
 });
 //# sourceMappingURL=server.js.map
--- NodejsWebApp1/server.ts
+++ NodejsWebApp1/server.ts
@@ -7,17 +7,26 @@
 
 
 var express = require('express')
-var app = express()
+var pem = require('pem')
+var https = require('https')
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 1337
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 443
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-app.get('/', function (req, res) {
-    res.send('Hello World from Express!!!')
+// had to set set OPENSSL_CONF=c:/libs/openssl-0.9.8k/openssl.cnf to my Git's SSL folder on Windows. 
+
+
+pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+
+    var app = express()
+
+    app.get('/', function (req, res) {
+        res.send('Hello World from Express!!!')
+    })
+
+    https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(server_port, server_ip_address, function () {
+        console.log('Example app listening at http://%s:%s', server_ip_address, server_port)
+    })
 })
 
-var server = app.listen(server_port, server_ip_address, function () {
-    
-    console.log('Example app listening at http://%s:%s', server_ip_address, server_port)
 
-})
