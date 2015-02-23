b018a33a41b94f775adb4c73a602a7d8702733e4
srb- <sbeath@gmail.com>
2015-02-23 13:44:32 -0500

    OK, added in JS

---
+++ NodejsWebApp1/TestModule.js
@@ -0,0 +1,8 @@
+var TestClass = (function () {
+    function TestClass(firstname) {
+        this.FirstName = firstname;
+    }
+    return TestClass;
+})();
+exports.TestClass = TestClass;
+//# sourceMappingURL=TestModule.js.map
